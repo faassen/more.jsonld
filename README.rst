7a6bfbd1697b34638db7f53ac50136223fa2b56d
faassen <faassen@startifact.com>
2014-09-01 13:41:16 +0200

    Initial empty project.

---
+++ README.rst
@@ -0,0 +1,10 @@
+more.jsonld: JSON-LD support for Morepath
+=========================================
+
+more.jsonld adds a few facilities for supporting `JSON-LD`_ (JSON for
+Linked Data) to Morepath_.
+
+.. _`JSON-LD`: http://json-ld.org/
+
+.. _Morepath: http://morepath.readthedocs.org
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.txt').read())
+
+setup(name='more.jsonld',
+      version='0.1.dev0',
+      description="JSON-LD support for Morepath",
+      long_description=long_description,
+      author="Martijn Faassen",
+      author_email="faassen@startifact.com",
+      keywords='morepath jsonld rest',
+      license="BSD",
+      url="http://pypi.python.org/pypi/more.jsonld",
+      namespace_packages=['more'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'morepath >= 0.4',
+        ],
+      extras_require = dict(
+        test=['pytest >= 2.0',
+              'pytest-cov',
+              'WebTest >= 2.0.14'],
+        ),
+      )
