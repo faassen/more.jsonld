27992ab737cb5d86a5d554f461cb83250b87f564
faassen <faassen@startifact.com>
2014-09-01 14:51:17 +0200

    Basic story for id works now.

--- more/jsonld/app.py
+++ more/jsonld/app.py
@@ -1,5 +1,11 @@
 import morepath
 from morepath.directive import JsonDirective
+import reg
+
+
+@reg.generic
+def ld_id(request, self):
+    return None
 
 
 class JsonldApp(morepath.App):
@@ -8,4 +14,28 @@ class JsonldApp(morepath.App):
 
 @JsonldApp.directive('jsonld')
 class JsonldDirective(JsonDirective):
-    pass
+    def perform(self, registry, obj):
+        def view(self, request):
+            result = obj(self, request)
+            if '@id' not in result:
+                id = ld_id(request, self, lookup=request.lookup)
+                if id is not None:
+                    result['@id'] = id
+            return result
+        super(JsonldDirective, self).perform(registry, view)
+
+
+@JsonldApp.directive('ld_id')
+class LdIdDirective(morepath.Directive):
+    def __init__(self, app, model):
+        super(LdIdDirective, self).__init__(app)
+        self.model = model
+
+    def identifier(self, app):
+        return (self.model,)
+
+    def perform(self, registry, obj):
+        def f(request, model):
+            return obj(model, request)
+        registry.register(ld_id, [morepath.Request, self.model], f)
+
--- more/jsonld/tests/test_jsonld.py
+++ more/jsonld/tests/test_jsonld.py
@@ -4,6 +4,10 @@ import more.jsonld
 from webtest import TestApp as Client
 
 
+def setup_module(module):
+    morepath.disable_implicit()
+
+
 def test_noop():
     config = morepath.setup()
     config.scan(more.jsonld)
@@ -28,3 +32,32 @@ def test_noop():
     r = c.get('/')
     assert r.json == {}
 
+
+def test_id():
+    config = morepath.setup()
+    config.scan(more.jsonld)
+
+    class app(JsonldApp):
+        testing_config = config
+
+    class Foo(object):
+        def __init__(self, id):
+            self.id = id
+
+    @app.path(path='/foos/{id}', model=Foo)
+    def get_foo(id):
+        return Foo(id)
+
+    @app.jsonld(model=Foo)
+    def foo_default(self, request):
+        return {}
+
+    @app.ld_id(model=Foo)
+    def foo_id(self, request):
+        return request.link(self)
+
+    config.commit()
+
+    c = Client(app())
+    r = c.get('/foos/10')
+    assert r.json == { '@id': '/foos/10' }
